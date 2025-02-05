ab7ff780377952aa020a9fb23a8a9edd0ddf984b
Naveennuwan <68013538+Naveennuwan@users.noreply.github.com>
2025-02-05 09:31:24 +0530

    2025-02-05

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,7 +1,7 @@
 <!-- Navbar -->
 <nav class="navbar">
     <div class="navbar-left">
-        <a href="/" class="logo">User Management</a>
+        <!-- <a href="/" class="logo">User Management</a> -->
     </div>
 
     <div class="navbar-menu ">
@@ -20,5 +20,29 @@
             </div>
         @endcan
     </div>
+
+<div class="navbar-right">
+    <div class="online">
+        <img src="{{ asset('images/user-1.png') }}" class="nav-profile-img dropdown" onclick="toggleMenu()">
+    </div>
+
+    <div class="sub-menu-wrap" id="subMenu">
+        <div class="sub-menu">
+            <div class="user-info">
+                <img src="{{ asset('images/user-1.png') }}">
+                {{ Auth::user()->name ?? '' }}
+            </div>
+            <hr>
+            <a href="{{ url('/logout') }}" class="sub-menu-link"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                <img src="{{ asset('images/logout.png') }}">
+                <p>Logout</p> {{-- Logout --}}
+            </a>
+            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+            </form>
+        </div>
+    </div>
+</div>
 </nav>
 <!-- Navbar -->
